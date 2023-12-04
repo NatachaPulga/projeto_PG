a245407dddf384d0e2380ee41eabc7a5c919d99d
NatachaPulga <nppaa@iscte-iul.pt>
2023-12-04 11:58:41 +0000

    aaaaaaaaa

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ def main():
         def __init__(self, nome, interesses, artigos_disponiveis):
             pass
 
-        #Altera os interesses e/ou os artigos de um utilizador
+        #Altera os interesses e/ou os artigos de um utilizador.
         def editar_conta(self, novos_interesses, novos_artigos):
             pass
 
-        #Adiciona uma nova avaliação, podendo incluir um comentário
+        #Adiciona uma nova avaliação, podendo incluir um comentário.
         def deixar_avaliacao(self, estrelas, comentario):
             pass
 
         #Apresenta todas as avaliações e comentários.
         def listar_avaliacoes(self):
             pass
-        
+
         #Apresenta todos os interesses.
         def mostrar_interesses(self):
             pass
