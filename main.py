4a2c617f5970d6221012eaa8a678d7128296fc20
NatachaPulga <nppaa@iscte-iul.pt>
2023-12-04 11:08:44 +0000

    estrutura inicial

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 def main():
-    print("Hello World!")
+    class Utilizador:
+        #Construtor
+        def __init__(self, nome, interesses, artigos_disponiveis):
+            pass
+
+        #Altera os interesses e/ou os artigos de um utilizador
+        def editar_conta(self, novos_interesses, novos_artigos):
+            pass
+
+        #Adiciona uma nova avaliação, podendo incluir um comentário
+        def deixar_avaliacao(self, estrelas, comentario):
+            pass
+
+        #Apresenta todas as avaliações e comentários.
+        def listar_avaliacoes(self):
+            pass
+        #Apresenta todos os interesses.
+        def mostrar_interesses(self):
+            pass
+
+        #Apresenta todos os artigos.
+        def mostrar_artigos(self):
+            pass
+
+        #Altera o número de pycoins.
+        def alterar_pycoins(self, numero_pycoins):
+            pass
+
+        #Apresenta o número de pycoins.
+        def mostrar_pycoins(self):
+            pass
+    
+
 
 if __name__ == '__main__':
     main()
