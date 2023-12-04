d87a5d79eb5768ac5b7b42c27298aadcbe18808f
NatachaPulga <nppaa@iscte-iul.pt>
2023-12-04 10:49:18 +0000

    aaaa

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 def main():
-    print("Hello World!")
+    class Utilizador:
+        #Construtor
+        def __init__(self, nome, interesses, artigos_disponiveis):
+
+
+        #Altera os interesses e/ou os artigos de um utilizador
+        def editar_conta(self, novos_interesses, novos_artigos):
+            
+
+        #Adiciona uma nova avaliação, podendo incluir um comentário
+        def deixar_avaliacao(self, estrelas, comentario):
+
+
+        #Apresenta todas as avaliações e comentários
+        def listar_avaliacoes(self):
+
+
+        #Apresenta todos os interesses
+        def mostrar_interesses(self):
+
+
+        #Apresenta todos os artigos
+        def mostrar_artigos(self):
+
+
+        #Altera o número de pycoins
+        def alterar_pycoins(self, numero_pycoins):
+
+
+        #Apresenta o número de pycoins
+        def mostrar_pycoins(self):
+
+
+            
 
 if __name__ == '__main__':
     main()
