680d224b30b06a045e1b63d1b01ade708e9f8fde
NatachaPulga <nppaa@iscte-iul.pt>
2023-12-18 15:48:02 +0000

    v.1.

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 def main():
+    # Funcionalidades relativas a um utilizador
     class Utilizador:
         #Construtor
         def __init__(self, nome, interesses, artigos_disponiveis):
-            pass
+            self.nome = nome
+            self.interesses = interesses
+            self.artigos_disponiveis = artigos_disponiveis
+            # Lista para armazenar as avaliações.
+            self.avaliacoes = []
 
-        #Altera os interesses e/ou os artigos de um utilizador.
+        #Altera os interesses e/ou os artigos de um utilizador
         def editar_conta(self, novos_interesses, novos_artigos):
-            pass
+            self.interesses = novos_interesses
+            self.artigos_disponiveis = novos_artigos
+            print(f"A conta do usuário {self.nome} atualizada com sucesso!")
 
-        #Adiciona uma nova avaliação, podendo incluir um comentário.
+        #Adiciona uma nova avaliação, podendo incluir um comentário
         def deixar_avaliacao(self, estrelas, comentario):
-            pass
+            # O '\n' é para fazer uma quebra de linha.
+            self.avaliacoes.append({"estrelas": estrelas, "comentario": comentario})
+            print(f"Avaliação do usuário {self.nome}, foi registrada com sucesso!")
 
-        #Apresenta todas as avaliações e comentários.
+        #Apresenta todas as avaliações e comentários
         def listar_avaliacoes(self):
-            pass
+            # Para verificar se a lista 'self.avaliacoes' está vazia.
+            if not self.avaliacoes:
+                print(f"Ainda não há avaliações para {self.nome}.")
+            else:
+                # Loop que percorre cada item da lista 'self.avaliacoes' e atribui temporariamente a cada um dos itens à variável 'avaliacao'.
+                # A 'avaliacao' representa um dicionário que contém informações sobre uma avaliação específica, como estrelas e comentários associados a essa avaliação.
+                for avaliacao in self.avaliacoes:
+                    print(f"Usuário: {self.nome}.")
+                    # O '\n' é para fazer uma quebra de linha.
+                    # Em '{avaliacao['estrelas']}' e '{avaliacao['comentário']}' estamos extrair e exibir o número de estrelas da avaliação e o comentário, armazenada no dicionário 'avaliacao'.
+                    print(f"Avaliação: {avaliacao['estrelas']} estrelas. \n Comentário: {avaliacao['comentário']}.")
+
+
+
+
 
         #Apresenta todos os interesses.
         def mostrar_interesses(self):
