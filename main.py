1ff3fe02308c76a3cf15bc0d75a186824fecdb69
NatachaPulga <nppaa@iscte-iul.pt>
2023-12-19 11:05:14 +0000

    trabalharClassArtigo-BORA

--- main.py
+++ main.py
@@ -69,6 +69,61 @@ def main():
              print(self.pycoins)
     
 
+    #Funcionalidades relativas a um artigo
+    class Artigo:
+       #Construtor
+        def __init__(self, nome, preco, tipologia, quantidade):
+            self.nome = nome
+            self.preco = preco
+            self.tipologia = tipologia
+            self.quantidade = quantidade
+
+        #Altera o nome de um artigo para o novo nome recebido
+        def editar_nome(self, nome):
+            pass
+        
+        #Altera o preço de um artigo de acordo com a percentagem dada
+        def ajustar_preco(self, percentagem_alteracao):
+            pass
+        
+        #Altera o preço para o novo preço recebido
+        def editar_preco(self, preco):
+            pass
+        
+        #Apresenta o preço do artigo
+        def mostrar_preco(self):
+            pass
+        
+        #Altera a quantidade
+        def editar_quantidade(self, nova_quantidade):
+            pass
+        
+        #Apresenta a quantidade do artigo
+        def mostrar_quantidade(self):
+            pass
+        
+        #Altera a tipologia
+        def editar_tipo (self, novo_tipo):
+            pass
+        
+        #Apresenta a tipologia do artigo
+        def mostrar_tipo (self):    
+            pass
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
